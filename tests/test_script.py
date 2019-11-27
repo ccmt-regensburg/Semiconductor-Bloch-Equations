0d5977c166599fd04cbb2a8bc1e9dedbdb1f48f8
Jan Wilhelm <wij17778@gw.ngate.uni-regensburg.de>
2019-11-27 14:15:42 +0100

    first version of computing the absolute error

--- tests/test_script.py
+++ tests/test_script.py
@@ -19,10 +19,25 @@ def main():
        with open(filename_reference) as f_reference:
            count = 1
            for line in f:
-               print("Line {}: {}".format(count, line.strip()))
                fields = line.split()
                print(fields[0])
                print(fields[1])
+               value_test = float(fields[1])
+
+               count_reference = 1
+               for line_reference in f_reference:
+                   fields_reference = line_reference.split()
+
+                   # we have the -1 because there is the header with executing command
+                   # in the reference file
+                   if count == count_reference-1:
+                       print(fields_reference[1])
+                       value_reference = float(fields_reference[1])
+                       abs_error = np.abs(value_test - value_reference)
+                       rel_error = abs_error/np.abs(value_reference)
+                       print("abs_error =", abs_error)
+
+                   count_reference += 1
 
                count += 1
 #               rel_error = 
