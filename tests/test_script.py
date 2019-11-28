02e0807f57979db7c63db3c023fa591fdaa0eba6
Jan Wilhelm <wij17778@gw.ngate.uni-regensburg.de>
2019-11-28 14:54:01 +0100

    add reset option to the test_script.py to automatically generate the *.reference files while only having the first line (command to execute the SBE.py script) in the *.reference files

--- tests/test_script.py
+++ tests/test_script.py
@@ -1,12 +1,11 @@
 import numpy as np
-import os
-import pytest
+import os, pytest, argparse
 
 ######################################################################################################
 # THIS SCRIPT NEEDS TO BE EXECUTED IN THE MAIN GIT DIRECTORY BY CALLING python3 tests/test_script.py #
 ######################################################################################################
 
-def check_test(filename_reference):
+def check_test(filename_reference, args):
 
    threshold_rel_error = 1.0E-10
    threshold_abs_error = 1.0E-24
@@ -34,47 +33,61 @@ def check_test(filename_reference):
           "Number from current git", "Relative error", "Absolute error"))
    print ("")
 
-   with open(filename) as f:
-       count = 0
-       for line in f:
-           count += 1
-           fields = line.split()
-           value_test = float(fields[1])
+   # reset mode of the script
+   if args.reset:
+      os.system("sed -n -i '1p' "+filename_reference)
+      os.system("cat "+filename+" >> "+filename_reference)
 
-           with open(filename_reference) as f_reference:
+   # normal test mode if the script
+   else:
 
-               count_reference = 0
-               for line_reference in f_reference:
-                   count_reference += 1
-                   fields_reference = line_reference.split()
+      with open(filename) as f:
+          count = 0
+          for line in f:
+              count += 1
+              fields = line.split()
+              value_test = float(fields[1])
+   
+              with open(filename_reference) as f_reference:
+   
+                  count_reference = 0
+                  for line_reference in f_reference:
+                      count_reference += 1
+                      fields_reference = line_reference.split()
+   
+                      # we have the -1 because there is the header with executing command
+                      # in the reference file
+                      if count == count_reference-1:
+                          value_reference = float(fields_reference[1])
+   
+                          abs_error = np.abs(value_test - value_reference)
+                          rel_error = abs_error/np.abs(value_reference)
+   
+                          check_abs = abs_error < threshold_abs_error
+                          check_rel = rel_error < threshold_rel_error
+   
+                          print('{:<15} {:>25} {:>25} {:>25} {:>25}'.format(fields_reference[0], \
+                                value_reference, value_test, rel_error, abs_error))
+   
+                          assert check_abs or check_rel, \
+                                 "\n\nAbsolute and relative error of variable number "+str(count)+\
+                                 " compared to reference too big:"\
+                                 "\n\nRelative error: "+str(rel_error)+" and treshold: "+str(threshold_rel_error)+\
+                                 "\n\nAbsolute error: "+str(abs_error)+" and treshold: "+str(threshold_abs_error)
+   
+              f_reference.close()
 
-                   # we have the -1 because there is the header with executing command
-                   # in the reference file
-                   if count == count_reference-1:
-                       value_reference = float(fields_reference[1])
-
-                       abs_error = np.abs(value_test - value_reference)
-                       rel_error = abs_error/np.abs(value_reference)
-
-                       check_abs = abs_error < threshold_abs_error
-                       check_rel = rel_error < threshold_rel_error
-
-                       print('{:<15} {:>25} {:>25} {:>25} {:>25}'.format(fields_reference[0], \
-                             value_reference, value_test, rel_error, abs_error))
-
-                       assert check_abs or check_rel, \
-                              "\n\nAbsolute and relative error of variable number "+str(count)+\
-                              " compared to reference too big:"\
-                              "\n\nRelative error: "+str(rel_error)+" and treshold: "+str(threshold_rel_error)+\
-                              "\n\nAbsolute error: "+str(abs_error)+" and treshold: "+str(threshold_abs_error)
-
-           f_reference.close()
-
-   print("\n\nTest passed successfully.\n\n")
-
-   f.close()
+      print("\n\nTest passed successfully.\n\n")
+    
+      f.close()
 
 def main():
+
+   parser = argparse.ArgumentParser(description='Test script')
+   parser.add_argument('-reset',  default=False, action='store_true',   help='Flag to reset all *.reference files in ./tests. \
+                       Needed: Put all .reference files you want to reset/update in ./tests and insert the command to execute the main script in the first line of the .reference file. \
+                       The reset mode of this script will insert the lines of the test file after the first line (which contains the command to execute the main script).')
+   args = parser.parse_args()
 
    is_dir = os.path.isdir("./tests")
    dirpath = os.getcwd()
@@ -85,7 +98,7 @@ def main():
    for filename_reference in os.listdir("./tests"):
        if filename_reference.endswith(".reference"): 
            count += 1
-           check_test("./tests/"+filename_reference)
+           check_test("./tests/"+filename_reference, args)
            continue
        else:
            continue
