484aa2bd63452a3ad22414e67829382fc0f89555
Jan Wilhelm <wij17778@gw.ngate.uni-regensburg.de>
2019-12-26 21:03:03 +0100

    print dot P (w) and J(w) but it is strange

--- params.py
+++ params.py
@@ -3,25 +3,25 @@ import numpy as np
 
 # Brillouin zone parameters
 ##########################################################################
-Nk_in_path          = 200         # Number of kpoints in each of the two paths
+Nk_in_path          = 100         # Number of kpoints in each of the two paths
 rel_dist_to_Gamma   = 0.05        # relative distance (in units of 2pi/a) of both paths to Gamma
 a                   = 8.308       # Lattice spacing in atomic units!! (4.395 A)
 length_path_in_BZ   = np.pi/a       # 
 E_dir               = np.array([0.0, 1.0])   # normalized direction of the E-field and therefore, direction of the path
-scale_dipole        = 0.1         # phenomenological rescaling of the dipole moments to match the experiments
+scale_dipole        = 1.0         # phenomenological rescaling of the dipole moments to match the experiments
 
 # Driving field parameters
 ##########################################################################
-E0    = 6.0  # Pulse amplitude (MV/cm)
+E0    = 1.0  # Pulse amplitude (MV/cm)
 w     = 26.0  # Pulse frequency (THz)
 alpha = 20.0  # Gaussian pulse width (femtoseconds)
 align = 'M'   # Direction of field polarization ('M'=gamma-M, 'K'=gamma-K)
 
 # Time scales (all units in femtoseconds)
 ##########################################################################
-T2    = 500.0   # Phenomenological polarization damping time 
+T2    = 100.0   # Phenomenological polarization damping time 
 t0    = -1000    # Start time *pulse centered @ t=0, use t0 << 0
-tf    = 1000     # End time
+tf    = 2000     # End time
 dt    = 0.02     # Time step
 
 # Unit conversion factors
