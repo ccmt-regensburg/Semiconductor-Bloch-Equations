b26ea2840ba6c051dd15b49e894cd81492f593c0
Jan Wilhelm <wij17778@gw.ngate.uni-regensburg.de>
2019-12-27 11:42:21 +0100

    changed parameters towards more realisitic experimental conditions, but the result is not really clean

--- params.py
+++ params.py
@@ -3,16 +3,16 @@ import numpy as np
 
 # Brillouin zone parameters
 ##########################################################################
-Nk_in_path          = 100         # Number of kpoints in each of the two paths
-rel_dist_to_Gamma   = 0.05        # relative distance (in units of 2pi/a) of both paths to Gamma
+Nk_in_path          = 200         # Number of kpoints in each of the two paths
+rel_dist_to_Gamma   = 0.1         # relative distance (in units of 2pi/a) of both paths to Gamma
 a                   = 8.308       # Lattice spacing in atomic units!! (4.395 A)
-length_path_in_BZ   = np.pi/a       # 
+length_path_in_BZ   = 2.0*np.pi/a       # 
 E_dir               = np.array([0.0, 1.0])   # normalized direction of the E-field and therefore, direction of the path
 scale_dipole        = 1.0         # phenomenological rescaling of the dipole moments to match the experiments
 
 # Driving field parameters
 ##########################################################################
-E0    = 1.0  # Pulse amplitude (MV/cm)
+E0    = 5.0   # Pulse amplitude (MV/cm)
 w     = 25.0  # Pulse frequency (THz)
 alpha = 25.0  # Gaussian pulse width (femtoseconds)
 align = 'M'   # Direction of field polarization ('M'=gamma-M, 'K'=gamma-K)
