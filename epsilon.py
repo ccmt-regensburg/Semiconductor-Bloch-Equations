2e14fb80afc9f6c1d2306e92cc30271e4be720c1
Bachelor Evers <stephanzwicknagl@gmx.de>
2020-05-27 10:49:44 +0200

    add epsilon for 1 band

---
+++ epsilon.py
@@ -0,0 +1,63 @@
+import numpy as np
+import params
+#from numba import njit
+import matplotlib.pyplot as pl
+#from matplotlib import patches
+#from scipy.integrate import ode
+#from scipy.special import erf
+#from sys import exit
+
+def epsilon(params):
+
+    angle_inc_E_field = params.angle_inc_E_field
+    E_dir = np.array([np.cos(np.radians(angle_inc_E_field)), np.sin(np.radians(angle_inc_E_field))])
+    dk, kpnts, paths = mesh(params, E_dir)
+
+    epsilon = []
+
+    for i in range(0,100):
+
+        epsk = [paths[0,i,0],np.cos(paths[0,i,0])]
+        epsilon.append(epsk)
+
+    print(*epsilon)
+    
+
+    return epsilon 
+
+
+
+def mesh(params, E_dir):
+    Nk_in_path = params.Nk_in_path                    # Number of kpoints in each of the two paths
+    rel_dist_to_Gamma = params.rel_dist_to_Gamma      # relative distance (in units of 2pi/a) of both paths to Gamma
+    a = params.a                                      # Lattice spacing
+    length_path_in_BZ = params.length_path_in_BZ      #
+
+    alpha_array = np.linspace(-0.5 + (1/(2*Nk_in_path)), 0.5 - (1/(2*Nk_in_path)), num = Nk_in_path)
+    vec_k_path = E_dir*length_path_in_BZ
+
+    vec_k_ortho = 2.0*np.pi/a*rel_dist_to_Gamma*np.array([E_dir[1], -E_dir[0]])
+
+    # Containers for the mesh, and BZ directional paths
+    mesh = []
+    paths = []
+
+    # Create the kpoint mesh and the paths
+    for path_index in [-1, 1]:
+        # Container for a single path
+        path = []
+        for alpha in alpha_array:
+            # Create a k-point
+            kpoint = path_index*vec_k_ortho + alpha*vec_k_path
+
+            mesh.append(kpoint)
+            path.append(kpoint)
+
+        # Append the a1'th path to the paths array
+        paths.append(path)
+
+    dk = 1.0/Nk_in_path*length_path_in_BZ
+
+    return dk, np.array(mesh), np.array(paths)
+
+epsilon(params)
